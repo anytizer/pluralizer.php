3783ee915c9545a0df1c8a65d81839ef936f7d9f
server <server@example.com>
2018-02-21 14:03:17 -0700

    composer test

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -3,4 +3,4 @@ declare(strict_types=1);
 
 namespace tests;
 
-require_once("../src/libraries/classes/common/class.pluralizer.inc.php");
+require_once("../src/libraries/classes/common/pluralizer.php");
---
+++ tests/test.php
@@ -0,0 +1,12 @@
+<?php
+namespace tests;
+
+reqiore_once("vendor/autoload.php");
+
+use common\pluralizer;
+
+$word = "house";
+
+$pluralizer = new pluralizer();
+$word = $pluralizer->pluralize($word);
+echo $word;
